2ca5004eeb6690576df448360744256378ffe525
Emanuele Disco <emawind84@gmail.com>
2017-03-31 23:42:25 +0900

    update

---
+++ docker-auto.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOCKER_COMPOSE_VERSION="1.11.2"
+CONF_ARG="-f docker-compose.yml"
+SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPT_BASE_PATH"
+
+REGISTRY_URL="$REGISTRY_URL"
+if [ -z "$REGISTRY_URL" ]; then
+    REGISTRY_URL="$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')"
+fi
+
+if ! command -v docker-compose >/dev/null 2>&1; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+usage() {
+echo "Usage:  $(basename "$0") [MODE] [OPTIONS] [COMMAND]"
+echo 
+echo "Mode:"
+echo "  --ssl-nginx    Encrypted connection and basic auth with Nginx"
+echo "  --ssl          Standalone with encrypted connection and basic auth"
+echo "  --dev          Development mode no encryption and no basic auth"
+echo
+echo "Options:"
+echo "  --help         Show this help message"
+echo
+echo "Commands:"
+echo "  up             Start the services"
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+for i in "$@"
+do
+case $i in
+    --ssl-nginx)
+        CONF_ARG="-f docker-compose-nginx-ssl.yml"
+        shift
+        ;;
+    --ssl)
+        CONF_ARG="-f docker-compose-ssl.yml"
+        shift
+        ;;
+    --dev)
+        CONF_ARG="-f docker-compose-dev.yml"
+        shift
+        ;;
+    --help|-h)
+        usage
+        exit 1
+        ;;
+    *)
+        ;;
+esac
+done
+
+echo "Arguments: $CONF_ARG"
+echo "Command: $@"
+
+if [ "$1" == "up" ]; then
+    docker-compose $CONF_ARG pull
+    docker-compose $CONF_ARG build --pull
+    docker-compose $CONF_ARG up -d --remove-orphans
+    exit 0
+
+elif [ "$1" == "stop-all" ]; then
+    if [ -n "$(docker ps --format {{.ID}})" ]
+    then docker stop $(docker ps --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "remove-all" ]; then
+    if [ -n "$(docker ps -a --format {{.ID}})" ]
+    then docker rm $(docker ps -a --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "logs" ]; then
+    shift
+    docker-compose $CONF_ARG logs -f --tail 200 "$@"
+    exit 0
+
+fi
+
+docker-compose $CONF_ARG "$@"
